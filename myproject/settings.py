1871444f56223d38f954af3077f0fb0863c3df4b
Yogesh Kumar <yogeshkumar02691@gmail.com>
2020-04-27 15:43:56 +0530

    ignore .vs files

--- myproject/settings.py
+++ myproject/settings.py
@@ -85,7 +85,8 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
